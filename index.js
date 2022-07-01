4c5dbf2d7ba7c81b2a5c49eb13ec0b807869254d
Gerardo Acosta <gacostaf@gmail.com>
2022-07-01 11:16:39 -0500

    segunda

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
-app.listen(3000,() => {
-    console.log("Ready to listen!");
-})
+app.use("/test", (req, resp) => {
+  console.log("Incoming /test request");
+  resp.status(200).send("Calla perro infeliz... Ya te dije, y no repito, repito...");
+});
+
+app.listen(3000, () => {
+  console.log("Ready to listen!");
+});
